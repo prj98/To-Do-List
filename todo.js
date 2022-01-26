00f9f0010e64ac207e9eb592fd1c143ccee52a61
prj98 <60270595+prj98@users.noreply.github.com>
2022-01-26 10:04:59 +0530

    Update todo.js
    Final commit

--- todo.js
+++ todo.js
@@ -1,40 +1,46 @@
-// let todos=[];
-// todos=[{
-//     title:"",
-// complted:false,
-// }]
-//whenever add-->add item to array using push()--->show todolist();
-//wherever deleted=-->delete from array-->>showtodolist();
-// function addtodo(){
-//     const inputelement=document.getElementById("myInput");
-//     if(!inputelement.value){
-//         alert("input is req.");
-//     }else
-//     {
-//     const newtodo=document.createElement("li");
-//     newtodo.innerHTML=inputelement.value;
-//     const btn=document.createElement("button");
-//     btn.innerHTML="DELETE";
-//     newtodo.appendChild(btn);
-//     const unord=document.getElementById("myUL");
-//     unord.appendChild(newtodo);
-//     btn.addEventListener("click",()=>{
-//      newtodo.remove();
-//     });
-//     inputelement.value="";
-// }
-// }
-// addclose();
-// function addclose(){
-//     const listitem=document.getElementsByTagName("li");
-//     for(let i=0;i<listitem.length;i++){
-//         const item=listitem[i];
-//       const deletebutton=document.createElement("button");
-//       deletebutton.innerHTML="DELETE";
-//       deletebutton.addEventListener("click",()=>{
-//           item.remove();
-//       });
-//       item.appendChild(deletebutton); 
-//     }
-//     }
-// console.log("hii")
+function addtodo(){
+    const inputelement=document.getElementById("myInput");
+    if(!inputelement.value){
+        alert("input is req.");
+    }else
+    {
+    const newtodo=document.createElement("li");
+    newtodo.innerHTML=inputelement.value;
+    const btn=document.createElement("button");
+    btn.innerHTML="DELETE";
+    btn.addEventListener('mouseover',function(){
+    btn.style.backgroundColor='red';
+    })
+   btn.addEventListener('mouseleave',function(){
+   btn.style.backgroundColor='white';
+  })
+    newtodo.appendChild(btn);
+    btn.style.float="right";
+    const unord=document.getElementById("myUL");
+    unord.appendChild(newtodo);
+    btn.addEventListener("click",()=>{
+     newtodo.remove();
+    });
+    inputelement.value="";
+}
+}
+addclose();
+function addclose(){
+    const listitem=document.getElementsByTagName("li");
+    for(let i=0;i<listitem.length;i++){
+        const item=listitem[i];
+      const deletebutton=document.createElement("button");
+      deletebutton.innerHTML="DELETE";
+    deletebutton.addEventListener('mouseover',function(){
+   deletebutton.style.backgroundColor='red';
+    })
+deletebutton.addEventListener('mouseleave',function(){
+   deletebutton.style.backgroundColor='white';
+  })
+      deletebutton.addEventListener("click",()=>{
+          item.remove();
+      });
+      item.appendChild(deletebutton); 
+      deletebutton.style.float="right";
+    }
+    }
